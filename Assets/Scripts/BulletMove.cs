dc662707386089e2b19254d3b7af2283c641467c
NeonixGardecier <110261960+NeonixGardecier@users.noreply.github.com>
2025-04-03 10:09:04 +0100

    Layered Shooting

--- Assets/Scripts/BulletMove.cs
+++ Assets/Scripts/BulletMove.cs
@@ -3,9 +3,35 @@ using UnityEngine;
 public class BulletMove : MonoBehaviour
 {
     public float speed;
+
+    public bool isHoming = false;
+    public Transform homingTarget;
     
     void Update()
     {
+        if (!isHoming)
+        {
+            Vector3 offset = new Vector3(0,-0.2f,0);
+            Vector3 spherePos = transform.position + (Vector3.forward * 3) + offset;
+
+            Collider[] hitColliders = Physics.OverlapSphere(spherePos, 3);
+
+            foreach (var hitCollider in hitColliders)
+            {
+                if (hitCollider.gameObject.tag == "Enemy")
+                {
+                    homingTarget = hitCollider.gameObject.transform;
+                    isHoming = true;
+                }
+            }
+        }
+
+        if (isHoming)
+        {
+            transform.LookAt(homingTarget);
+        }
         transform.Translate(Vector3.forward * speed * Time.deltaTime);        
     }
+
+
 }
